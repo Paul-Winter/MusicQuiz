75e69bf60c375a7888e01b53b4fb4000d15cbb90
Paul Winter <zimin.pavel.v@gmail.com>
2020-08-31 18:26:54 +0300

    created form, added buttons, defined exit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MusicQuiz
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnGame = new System.Windows.Forms.Button();
+            this.btnParams = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGame
+            // 
+            this.btnGame.Location = new System.Drawing.Point(110, 43);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(151, 55);
+            this.btnGame.TabIndex = 0;
+            this.btnGame.Text = "ИГРА";
+            this.btnGame.UseVisualStyleBackColor = true;
+            // 
+            // btnParams
+            // 
+            this.btnParams.Location = new System.Drawing.Point(110, 161);
+            this.btnParams.Name = "btnParams";
+            this.btnParams.Size = new System.Drawing.Size(151, 55);
+            this.btnParams.TabIndex = 1;
+            this.btnParams.Text = "НАСТРОЙКИ";
+            this.btnParams.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(110, 274);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(151, 55);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "ВЫХОД";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(364, 382);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParams);
+            this.Controls.Add(this.btnGame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(380, 420);
+            this.MinimumSize = new System.Drawing.Size(380, 420);
+            this.Name = "MainForm";
+            this.Text = " Угадай мелодию!";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Button btnParams;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
